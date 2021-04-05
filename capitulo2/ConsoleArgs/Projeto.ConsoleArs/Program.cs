f8c87d35d0196e5c743b647a3927440e5959a9b3
thiagosilr <thiago.dasilvaresende@gmail.com>
2021-04-05 08:09:58 -0300

    pagina83

---
+++ capitulo2/ConsoleArgs/Projeto.ConsoleArs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Projeto.ConsoleArs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
